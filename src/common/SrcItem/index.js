f817f1131d54335fe1a402739460c167023a18d6
nhatha32 <ng.nhatha32@gmail.com>
2024-04-02 21:41:54 +0700

    fix conflict

--- src/common/SrcItem/index.js
+++ src/common/SrcItem/index.js
@@ -98,7 +98,12 @@ export default function SrcItem({ value = [], grid = [] }) {
           icon={<FontAwesomeIcon icon={icon.ellipsisVertical} />}
           onClick={() => setOpenModal(index)}
         />
-        {modal[index] && <BreadCrumbModal ctnStyles="br-15 br-TopRight-2" save={save} setSave={setSave} handleChangeSave={handleChangeSave} isFolder={value[1].type==="file"? false: true} infoItm={value[1].id}/>}
+        {modal[index] && <BreadCrumbModal 
+          ctnStyles="br-15 br-TopRight-2" 
+          save={save} setSave={setSave} 
+          handleChangeSave={handleChangeSave} 
+          isFolder={value[1].type==="file"? false: true} 
+          infoItm={value[1].id}/>}
       </div>
     );
   };
